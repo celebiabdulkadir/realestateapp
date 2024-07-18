5436633799db112f7156d9038f48cec22550e278
celebiabdulkadir <abdulkadircelebi.ac@gmail.com>
2024-07-18 16:41:11 +0300

    CORS enabled

--- auth/src/main/java/com/abdulkadir/auth/config/AuthConfig.java
+++ auth/src/main/java/com/abdulkadir/auth/config/AuthConfig.java
@@ -3,6 +3,7 @@ package com.abdulkadir.auth.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -34,4 +35,5 @@ public class AuthConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
+
 }
--- gateway/src/main/java/com/abdulkadir/gateway/config/GatewayConfig.java
+++ gateway/src/main/java/com/abdulkadir/gateway/config/GatewayConfig.java
@@ -22,4 +22,5 @@ public class GatewayConfig {
                         .uri("lb://advert"))
                 .build();
     }
+
 }
